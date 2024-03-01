cf97245b7e8039dbe38cbd9313bfd867d2944886
Pablo Vasconcelos <pablo.vasconcelos.cefet@gmail.com>
2024-03-01 00:10:08 -0300

    Finalizado HeroesBet PC

--- script/script.js
+++ script/script.js
@@ -13,79 +13,92 @@ function loadCards() {
 
         for (casa of data) {
 
+            //callback
+            const createButtonEventListener = (linkAfiliado) => {
+                return () => {
+                    window.open(linkAfiliado, '_blank');
+                };
+            };
+
             /*cards-element*/
             const cardElement = document.createElement('div')
             cardElement.classList.add('card-element')
-            
-                /*figure*/
-                const figure = document.createElement('figure')
-                figure.classList.add('card-figure')
-                cardElement.appendChild(figure)
-            
-                    /* card-logo */
-                    const imgLogo = document.createElement('img')
-                    imgLogo.classList.add('card-logo')
-                    imgLogo.src = `../src/Site/logos/${casa.logo}`
-                    imgLogo.alt = casa.nome
-                    figure.appendChild(imgLogo)
 
-                /* rate-container */
-                const rateContainer = document.createElement('div')
-                rateContainer.classList.add('rate-container')
-                cardElement.appendChild(rateContainer)
+            const square = document.createElement('div')
+            square.classList.add('square')
+            square.style = " background-color: black;"
+            cardElement.appendChild(square)
 
-                    /* card-title */
-                    const cardTitle = document.createElement('span')
-                    cardTitle.classList.add('card-title')
-                    cardTitle.textContent = 'Avaliações'
-                    rateContainer.appendChild(cardTitle)
+            /*figure*/
+            const figure = document.createElement('figure')
+            figure.classList.add('card-figure')
+            cardElement.appendChild(figure)
 
-                     /* card-title */
-                     const cardRate = document.createElement('p')
-                     cardRate.classList.add('card-rate')
-                     cardRate.textContent = casa.avaliacao
-                     rateContainer.appendChild(cardRate)
-                
-                /* dep-min-container */                
-                const depositoContainer = document.createElement('div')
-                depositoContainer.classList.add('dep-min-container')
-                cardElement.appendChild(depositoContainer)
+            /* card-logo */
+            const imgLogo = document.createElement('img')
+            imgLogo.classList.add('card-logo')
+            imgLogo.src = `../src/Site/logos/${casa.logo}`
+            imgLogo.alt = casa.nome
+            figure.appendChild(imgLogo)
 
-                     /* card-title */
-                     const cardTitle2 = document.createElement('span')
-                     cardTitle2.classList.add('card-title')
-                     cardTitle2.textContent = 'Deposito Mínimo'
-                     depositoContainer.appendChild(cardTitle2)
- 
-                      /* card-title */
-                      const cardText = document.createElement('p')
-                      cardText.classList.add('card-text')
-                      cardText.textContent = `${casa.deposito_min} BRL`
-                      depositoContainer.appendChild(cardText)
+            /* rate-container */
+            const rateContainer = document.createElement('div')
+            rateContainer.classList.add('rate-container')
+            cardElement.appendChild(rateContainer)
 
-                /* saq-min-container */                
-                const saqueContainer = document.createElement('div')
-                saqueContainer.classList.add('saq-min-container')
-                cardElement.appendChild(saqueContainer)
+            /* card-title */
+            const cardTitle = document.createElement('span')
+            cardTitle.classList.add('card-title')
+            cardTitle.textContent = 'Avaliações'
+            rateContainer.appendChild(cardTitle)
 
-                     /* card-title */
-                     const cardTitle3 = document.createElement('span')
-                     cardTitle3.classList.add('card-title')
-                     cardTitle3.textContent = 'Saque Mínimo'
-                     saqueContainer.appendChild(cardTitle3)
- 
-                      /* card-title */
-                      const cardText2 = document.createElement('p')
-                      cardText2.classList.add('card-text')
-                      cardText2.textContent = `${casa.saque_min} BRL`
-                      saqueContainer.appendChild(cardText2)
+            /* card-title */
+            const cardRate = document.createElement('p')
+            cardRate.classList.add('card-rate')
+            cardRate.textContent = casa.avaliacao
+            rateContainer.appendChild(cardRate)
 
-                /* play-button */    
-                const buttonPlay = document.createElement('button')
-                buttonPlay.classList.add('play-button')
-                buttonPlay.textContent = 'QUERO JOGAR!'                
-                cardElement.appendChild(buttonPlay)
-            
+            /* dep-min-container */
+            const depositoContainer = document.createElement('div')
+            depositoContainer.classList.add('dep-min-container')
+            cardElement.appendChild(depositoContainer)
+
+            /* card-title */
+            const cardTitle2 = document.createElement('span')
+            cardTitle2.classList.add('card-title')
+            cardTitle2.textContent = 'Deposito Mínimo'
+            depositoContainer.appendChild(cardTitle2)
+
+            /* card-title */
+            const cardText = document.createElement('p')
+            cardText.classList.add('card-text')
+            cardText.textContent = `${casa.deposito_min} BRL`
+            depositoContainer.appendChild(cardText)
+
+            /* saq-min-container */
+            const saqueContainer = document.createElement('div')
+            saqueContainer.classList.add('saq-min-container')
+            cardElement.appendChild(saqueContainer)
+
+            /* card-title */
+            const cardTitle3 = document.createElement('span')
+            cardTitle3.classList.add('card-title')
+            cardTitle3.textContent = 'Saque Mínimo'
+            saqueContainer.appendChild(cardTitle3)
+
+            /* card-title */
+            const cardText2 = document.createElement('p')
+            cardText2.classList.add('card-text')
+            cardText2.textContent = `${casa.saque_min} BRL`
+            saqueContainer.appendChild(cardText2)
+
+            /* play-button */
+            const buttonPlay = document.createElement('button')
+            buttonPlay.classList.add('play-button')
+            buttonPlay.textContent = 'QUERO JOGAR!'
+            buttonPlay.addEventListener('click', createButtonEventListener(casa.link_afiliado))
+            cardElement.appendChild(buttonPlay)
+
             cardContainer.appendChild(cardElement)
             console.log(casa.nome);
         }
